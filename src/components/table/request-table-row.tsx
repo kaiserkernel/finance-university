9937fddb61b37e356e8238a9d5a1ccbcd7779e9e
kaiserkernel <kaiserkernel84@gmail.com>
2025-02-23 15:50:18 +0100

    fix: accept invoice btn caption

--- src/components/table/request-table-row.tsx
+++ src/components/table/request-table-row.tsx
@@ -329,7 +329,7 @@ export function UserTableRow({
 									variant="outlined"
 								>
 									<Iconify icon="solar:check-circle-broken" />
-									Accept {(prevState === 'reviewed' && user.role !== 'finance') && `(2)`}
+									Accept {(prevState === 'reviewed' && user.role !== 'finance') && `Inovice`}
 								</Button>
 
 								{
